94d64fe34f26a0c7609bde556f6f38131fc25f93
Esteban <estebanmc2912@gmail.com>
2020-09-25 12:18:43 -0500

    Cambio visual del ListView Creado, ahora con mejoras en posicion, lineas divisoras, mejor tamaño y estilo

--- lib/main.dart
+++ lib/main.dart
@@ -18,16 +18,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RandomWords extends StatefulWidget {
+class RandomWords extends StatefulWidget { // Creador del estado interactivo
   @override
   State<StatefulWidget> createState() {
     return RandomWordsState();
-    throw UnimplementedError();
   }
 }
 
-class RandomWordsState extends State<RandomWords>{
-  final _suggestions = <WordPair>[]; // el raya al piso significa que un atributo es privado
+class RandomWordsState extends State<RandomWords>{  // Creador del diseño de la aplicación que hereda el estado del antes mencionado
+  final _suggestions = <WordPair>[]; // el raya al piso significa que un atributo es privado, lista de palabras
+  final _biggerFont = const TextStyle( fontSize: 16); // creación de variable para definir el tamaño
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,20 +39,31 @@ class RandomWordsState extends State<RandomWords>{
     );
   }
 
-  Widget _buildSuggestions (){
+  Widget _buildSuggestions (){ // creador del List View
     return ListView.builder(
+        padding: const EdgeInsets.all(16.0), // atributo para acomodar mejor la lista según el dispositivo
         itemBuilder: (context, i) {
+
+          if (i.isOdd){
+            return Divider(); // si el item (listView) es par retorna un divisor, hacer la prueba poniendolo falso para entender
+          }
+
           if (i >=_suggestions.length) { //para que la lista mostrada sea interminable (infinita)
             _suggestions.addAll(generateWordPairs().take(10));
                 }
+
+
           return _buildRow(_suggestions[i]);
         },
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair){ // creador de cada fila
     return ListTile(
-     title: Text(pair.asPascalCase),
+     title: Text(
+         pair.asPascalCase,
+         style: _biggerFont, // llamamos la variable creada anteriormente y acá le damos estilo
+     ),
     );
   }
 
